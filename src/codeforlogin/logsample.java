078040b225b4e0d0fef0a867b8f63e713d6654d8
cjeevankumar <cjevankumar90@gmail.com>
2017-09-28 16:10:51 +0530

    todays work

---
+++ src/codeforlogin/logsample.java
@@ -0,0 +1,21 @@
+package codeforlogin;
+
+public class logsample {
+	
+	 String userame = null;
+	 String password = null;
+	    
+	    public String getCode() {
+	        return userame;
+	    }
+	    public void setCode(String userame) {
+	        this.userame = userame;
+	    }
+	    public String getName() {
+	        return password;
+	    }
+	    public void setName(String password) {
+	        this.password = password;
+	    }
+
+}
